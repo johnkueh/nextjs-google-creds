1e82e00d055dcc3ae956a4f061ee76d2114ae10a
johnkueh <john.kueh@gmail.com>
2021-03-19 12:07:22 +1100

    feat: able to use remote image urls

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,30 @@
 import { processImage } from "../shared/processImage";
 
-export default function Home(props) {
+export default function Home({ imageUrl, data }) {
   return (
     <div>
       <div>Google Vision API with credentials</div>
       <hr />
       <div>
         <h3>Image</h3>
-        <img
-          width="500px"
-          src="https://i3.kym-cdn.com/photos/images/facebook/000/242/592/1c8.jpg"
-        />
+        <img width="500px" src={imageUrl} />
       </div>
       <hr />
       <h3>Data</h3>
-      <div>{JSON.stringify(props.data)}</div>
+      <div>{JSON.stringify(data)}</div>
     </div>
   );
 }
 
-export async function getServerSideProps(context) {
-  const data = await processImage(
-    "http://i3.kym-cdn.com/photos/images/facebook/000/242/592/1c8.jpg"
-  );
+export async function getServerSideProps({ query }) {
+  const imageUrl =
+    query.image ??
+    "http://i3.kym-cdn.com/photos/images/facebook/000/242/592/1c8.jpg";
+  const data = await processImage(imageUrl);
 
   return {
     props: {
+      imageUrl,
       data,
     },
   };
